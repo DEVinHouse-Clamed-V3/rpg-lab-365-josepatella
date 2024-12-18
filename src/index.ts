82c382bfd6fb933247c0e700534f11e50f2c3f65
Patella <jfpatella@gmail.com>
2024-12-18 11:20:33 -0300

    Instanciado objetos Arma, Inimigo e personagem. Jogo simulado.

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ class Personagem {
   atacar(oponente: Inimigo) {
     const chanceDesucesso = Math.random()
     if (chanceDesucesso < 0.5) {
-      console.log("Errou o ataque!")
+      console.log(`${this.getNome()} errou o ataque no ${oponente.getNome()}!`)
     } else {
       const dano = this.calcularDano()
       this.receberDano(dano, oponente)
@@ -86,7 +86,7 @@ class Personagem {
 
   receberDano(dano: number, oponente: Personagem) {
     oponente.vida -= dano
-    console.log(`Você foi atingido e teve ${dano} pontos de dano!`);
+    console.log(`${oponente.nome} foi atingido pelo ${this.nome} e teve ${dano} pontos de dano!`);
     if (oponente.vida <= 0) {
       console.log(`Você foi derrotado!`)
       oponente.vida = 0
@@ -108,7 +108,7 @@ class Inimigo extends Personagem {
     atacar(oponente: Personagem) {
         const chanceDesucesso = Math.random()
         if (chanceDesucesso < 0.2) {
-          console.log("Errou o ataque!")
+          console.log(`${this.getNome()} errou o ataque no ${oponente.getNome()}!`)
         } else {
           const dano = this.calcularDano()
           this.receberDano(dano, oponente)
@@ -118,9 +118,41 @@ class Inimigo extends Personagem {
       comportamentoAleatorio(jogador: Personagem){
         const chanceDesucesso = Math.random()
         if (chanceDesucesso < 0.5) {
-          console.log("Observar o oponente...")
+          console.log(`${this.getNome()} observar o oponente ${jogador.getNome()}`)
         } else {
           this.atacar(jogador)
         }
       }
 }
+
+const espadaLonga = new Arma(
+    'Espada Longa',
+    30,
+    'Uma espada afiada e resistente, ideal para combates corpo a corpo.'
+  );
+  
+  const arcoCurto = new Arma(
+    'Arco Curto',
+    25,
+    'Um arco leve e rápido, excelente para ataques à distância.'
+  );
+  
+  const machadoDeBatalha = new Arma(
+    'Machado de Batalha',
+    40,
+    'Um machado pesado, capaz de causar grandes danos com golpes poderosos.'
+  );
+
+const guerreiro = new Inimigo("Guerreiro Valente", 100, 20, machadoDeBatalha);
+const mago = new Inimigo("Mago Sábio", 60, 15, espadaLonga);
+const arqueiro = new Inimigo("Arqueiro Ágil", 80, 10, arcoCurto);
+const curandeiro = new Inimigo("Curandeira Gentil", 70, 5, null);
+const ladrao = new Inimigo("Ladrão Sorrateiro", 90, 12, null);
+
+const cavaleiro = new Personagem("Cavaleiro rápido", 100, 30, espadaLonga);
+
+cavaleiro.atacar(guerreiro)
+mago.atacar(cavaleiro)
+arqueiro.comportamentoAleatorio(cavaleiro)
+cavaleiro.atacar(ladrao)
+curandeiro.comportamentoAleatorio(cavaleiro)
